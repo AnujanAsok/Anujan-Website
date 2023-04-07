ac48d3a2b470f2ccddf6d34204910796240bcef5
AnujanAsok <anujan.asokkumar@gmail.com>
2023-04-07 16:11:57 -0400

    checking v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
-
-
-function addNumbers1 (x, y) {
+function addNumbers1(x, y) {
   console.log(x, y);
   return x + y;
 }
@@ -15,37 +13,36 @@ const addNumbers2 = (x, y) => x + y;
 const addNumbers3 = (x, y) => {
   console.log(x, y);
   return x + y;
-}
+};
 
 const getpokemon = async () => {
-  const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const response = await axios.get(
+    "https://pokeapi.co/api/v2/pokemon?limit=151"
+  );
   console.log(response.data);
   return response;
-}
+};
 
 const App = () => {
-  const imageurl="https://s7d2.scene7.com/is/image/TWCNews/rain_showers_jpg"
-  const thulasiage=10;
-  const anujanage=15;
-  const lathanage=25;
-  const average=(thulasiage+anujanage+lathanage)/3
-  const rounded=Math.round(average);
+  const imageurl = "https://s7d2.scene7.com/is/image/TWCNews/rain_showers_jpg";
+  const thulasiage = 10;
+  const anujanage = 15;
+  const lathanage = 25;
+  const average = (thulasiage + anujanage + lathanage) / 3;
+  const rounded = Math.round(average);
   getpokemon();
   console.log("I was here.");
- 
+
   // console.log(pokemon.results[73].name);
   return (
     <div className="App">
       <header className="App-header">
-      <h1>Welcome to Anujan's Website</h1>
-        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Welcome to Anujan's Website V2</h1>
+        <img src={logo} className="App-logo" alt="logo" />
         <img src={imageurl} alt="rain" />
-        
+
+        <p>This is all about Anujan</p>
         <p>
-          This is all about Anujan
-        </p>
-        <p>
-          
           {/* {pokemon.results.map(p => {
             return p.name + ", ";
           })} */}
@@ -63,12 +60,12 @@ const App = () => {
           href="https://justflipacoin.com/"
           target="_blank"
           rel="noopener noreferrer"
-          >
-            Do A Flip
+        >
+          Do A Flip
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
